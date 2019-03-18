c388bc1fe70f4ab30ab0208b6a612eea24021d53
Nguyen Quang Vu <bkv1409@gmail.com>
2019-03-18 10:08:13 +0700

    update schema 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\Resource;
 
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
         Resource::withoutWrapping();
     }
 
